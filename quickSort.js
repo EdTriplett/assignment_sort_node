cfe9c85d89caf7059ae7fdbda71b12693c137c0d
Stephanie Barker <song_mage@yahoo.com>
2017-09-26 14:00:36 -0400

    quicksort started

--- quickSort.js
+++ quickSort.js
@@ -1,1 +1,22 @@
-quickSort.js
+const quickSort = arr => {
+	if (arr.length < 2) return arr;
+
+	let pivot = arr[arr.length - 1];
+	let pivotPoint = 0;
+	let sortedPoint = 0;
+
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] <= pivot) {
+			[arr[i], arr[pivotPoint]] = [arr[pivotPoint], arr[i]];
+			pivotPoint++;
+		}
+
+		sortedPoint++;
+	}
+
+	[arr[arr.length - 1], arr[pivotPoint]] = [arr[pivotPoint], pivot];
+
+	return arr;
+};
+
+console.log(quickSort([6, 5, 9, 8, 7, 3, 1, 2, 4]));
